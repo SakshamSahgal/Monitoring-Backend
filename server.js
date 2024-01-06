2e3c4bb18c10a69b6f3eb3e7f9518e4cb4688cfb
Saksham Sahgal <73836766+SakshamSahgal@users.noreply.github.com>
2024-01-06 20:13:00 +0530

    working fs

--- server.js
+++ server.js
@@ -4,22 +4,22 @@ require("dotenv").config()
 const port = process.env.PORT || 8080
 const cors = require("cors")
 const path = require('path');
-const {connectDB} = require("./MongoOperations.js")
-const {hasAccess} = require("./Auth/Middlewares.js")
+const { connectDB } = require("./MongoOperations.js")
+const { hasAccess } = require("./Auth/Middlewares.js")
 const fs = require("fs")
 
 // for cross origin resource sharing
-app.use(cors())      
+app.use(cors())
 // for parsing application/json
-app.use(express.json()) 
+app.use(express.json())
 // making the build folder public
-app.use(express.static(path.join(__dirname, 'build'))); 
+app.use(express.static(path.join(__dirname, 'build')));
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`)
   connectDB()
   //ensuring that the uploads folder exists
-  if(!fs.existsSync(path.join(__dirname, 'uploads'))) 
+  if (!fs.existsSync(path.join(__dirname, 'uploads')))
     fs.mkdirSync(path.join(__dirname, 'uploads'));
 })
 
@@ -30,7 +30,7 @@ app.listen(port, () => {
 //this route is used to validate the token, it is called when a page logs in, it checks if the token is valid
 //if it is valid then it sends a response to the client - "Valid token"
 //the validation is done using the hasAccess function
-app.get("/validateToken",hasAccess, (req, res) => {
+app.get("/validateToken", hasAccess, (req, res) => {
   res.json({
     success: true,
     message: "Valid token"
@@ -42,3 +42,4 @@ require("./Archiever.js")(app)
 require("./getSizeOnDisk.js")(app)
 require("./serveTarget.js")(app)
 require("./upload.js")(app)
+require("./serveAdmin.js")(app)
